0d8b53f15d5f609dad6f9d27cf72ffaac123db3a
Eric Pedley <ericpedley@gmail.com>
2021-05-02 17:47:02 -0700

    figured out how to go from request to discord message

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import os
 import discord
 from dotenv import load_dotenv
 
+import requests
+from io import BytesIO
+#https://stackoverflow.com/a/22393884
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 MY_GUILD = os.getenv('MY_GUILD')
@@ -15,9 +18,9 @@ async def on_ready():
         if guild.name==MY_GUILD:
             for channel in guild.channels:
                 if channel.name=="bot-usage":
-                    myFile = open("videos/bruh moment.mp4",'rb')
-                    await channel.send(content="bruh",file=discord.File(myFile))
-                    myFile.close()
+                    u = requests.get("https://cdn.discordapp.com/attachments/789208552978907206/838570682102579200/videos_bruh_moment.mp4")
+                    tempFile = BytesIO(u.content)
+                    await channel.send(content="bruh",file=discord.File(tempFile,filename="ericisawesome.mp4"))#https://discordpy.readthedocs.io/en/stable/api.html#discord.File
     print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
