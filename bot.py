9971d052d6a93cc8603c5b40d4348877bc0c73bf
Eric Pedley <ericpedley@gmail.com>
2021-05-03 23:27:38 -0700

    made it work for instagram link (i think) it should work but I'm getting 429 errors, which might be because I'm making too many requests but also might be because I'm doing http requests not from a browser

--- bot.py
+++ bot.py
@@ -1,26 +1,32 @@
 # bot.py
-import os
 
 import discord
 from dotenv import load_dotenv
-
+import re#regular expressions
 import requests
 from io import BytesIO
 #https://stackoverflow.com/a/22393884
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-MY_GUILD = os.getenv('MY_GUILD')
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name==MY_GUILD:
-            for channel in guild.channels:
-                if channel.name=="bot-usage":
-                    u = requests.get("https://cdn.discordapp.com/attachments/789208552978907206/838570682102579200/videos_bruh_moment.mp4")
-                    tempFile = BytesIO(u.content)
-                    await channel.send(content="bruh",file=discord.File(tempFile,filename="ericisawesome.mp4"))#https://discordpy.readthedocs.io/en/stable/api.html#discord.File
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+        if message.content.find("instagram.com/p"):
+            webLink = re.findall(r"(www.instagram.com/p/...........)",message.content)[0]
+            webResponse = requests.get(f"https://{webLink}/")
+            webCode=webResponse.text
+            if(webResponse.status_code>=300):
+                print(f"error {webResponse.status_code}, wrote response html to file errorpage.html")
+                with open("errorpage.html",'w') as f:
+                    f.write(webCode)
+            else:
+                vidLink = re.findall(r'<meta property="og:video" content="(.+)"',webCode)[0]
+                vidResponse = requests.get(vidLink)
+                tempFile = BytesIO(vidResponse.content)
+                await message.channel.send(file=discord.File(tempFile,filename="epic_embed_clutch.mp4"))
 client.run(TOKEN)
